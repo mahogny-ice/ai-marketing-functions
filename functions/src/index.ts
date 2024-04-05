633af508e97a22447e2c7c1c4ecac371462679c4
Anton Klock <klock.anton@gmail.com>
2024-04-05 10:39:39 +0200

    moving functions to their own files and folders

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,37 +1,6 @@
-import { onRequest } from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
-export const helloWorld = onRequest((request, response) => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-});
+import { helloWorld } from "./utils/helloWorld";
+import { generatePodcastAd } from "./adGeneration/generatePodcastAd";
 
-export const mergeAudio = onRequest(async (request, response) => {
-    logger.info("Merge audio logs!", { structuredData: true });
+export { helloWorld, generatePodcastAd };
 
-    const mergeAudioUrl = "https://merge-media-dx3v2rbg6q-od.a.run.app/mergeaudio";
-
-    const { track1, track2 } = request.body;
-
-    if (!track1 || !track2) {
-        logger.error("Missing track1 or track2", { structuredData: true });
-        response.status(400).send("Missing track1 or track2. Please provide both tracks in the request body.");
-        return;
-    }
-
-    const mergeAudioResponse = await fetch(mergeAudioUrl, {
-        method: "POST",
-        body: JSON.stringify({ track1, track2 }),
-        headers: { "Content-Type": "application/json" },
-    });
-
-    if (!mergeAudioResponse.ok) {
-        logger.error("Error merging audio", { structuredData: true });
-        response.status(500).send("Error merging audio");
-        return;
-    } else {
-        logger.info("Successfully merged audio", { structuredData: true });
-        const res = await mergeAudioResponse.json();
-        response.json(res);
-    }
-});
