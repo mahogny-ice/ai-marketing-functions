c0a47fb9919481d068698492b27b8e51bb53a7e2
Jacob Laggar <jacob.laggar@gmail.com>
2024-04-08 14:29:42 +0200

    Update to firebase KEY

--- functions/src/apiCalls/callLeap.ts
+++ functions/src/apiCalls/callLeap.ts
@@ -2,7 +2,9 @@ import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import { Leap } from "@leap-ai/workflows";
 
-const LEAP_API_KEY = process.env.LEAP_API_KEY as string;
+const LEAP_API_KEY: string = process.env.FUNCTIONS_LEAP_API_KEY ?? (() => {
+    throw new Error("Required environment variable LEAP_API_KEY is not set");
+})();
 
 export const triggerLeapWorkflow = onRequest(async (request, response) => {
     const leap = new Leap({
@@ -11,15 +13,15 @@ export const triggerLeapWorkflow = onRequest(async (request, response) => {
 
     try {
         const body = request.body;
+
         logger.info("Received body: ", body, { structuredData: true });
 
         const leapResponse = await leap.workflowRuns.workflow({
             workflow_id: "wkf_FZIrfeC0AGcbTf",
-            input: body.input
+            input: body.input,
         });
 
         response.json(leapResponse.data);
-
     } catch (error) {
         logger.error("Error calling Leap API: ", error);
         response.status(500).send(error);
