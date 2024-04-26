dca84bb6c4872125b431512df248e4534c94db26
Anton Klock <anton@klockworks.se>
2024-04-26 11:23:58 +0200

    Updating some linting

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -16,19 +16,17 @@ module.exports = {
     parserOptions: {
         project: ["tsconfig.json", "tsconfig.dev.json"],
         sourceType: "module",
+        tsconfigRootDir: __dirname,
     },
     ignorePatterns: [
         "/lib/**/*", // Ignore built files.
     ],
-    plugins: [
-        "@typescript-eslint",
-        "import",
-    ],
+    plugins: ["@typescript-eslint", "import"],
     rules: {
         "object-curly-spacing": ["error", "always"],
         "quotes": ["error", "double"],
         "import/no-unresolved": 0,
         "indent": ["error", 4],
-        "max-len": ["error", { "code": 200 }],
+        "max-len": ["error", { code: 200 }],
     },
 };
