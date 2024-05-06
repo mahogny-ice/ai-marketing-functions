9e63606aa0b826612b6610a08762b8ccd25302e4
Jacob Laggar <jacob.laggar@gmail.com>
2024-05-06 11:37:48 +0200

    Update index.ts
    Update with generatespeech

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,10 +2,12 @@ import { processNewUser } from "./auth/processNewUser";
 import { generatePodcastAd } from "./adGeneration/generatePodcastAd";
 import { initiateMerge } from "./adGeneration/firestoreTriggers/initiateMerge";
 import { updateJobComponents } from "./adGeneration/updateComponents";
+import { generateSpeech } from "./apiCalls/generateSpeech";
 
 export {
     processNewUser,
     generatePodcastAd,
     initiateMerge,
     updateJobComponents,
+    generateSpeech,
 };
