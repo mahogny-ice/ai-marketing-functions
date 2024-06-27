2d455795fefcbd882d80884d5979a548d66514d0
Anton Klock <anton@klockworks.se>
2024-06-27 11:35:54 +0200

    Adding some logs

--- functions/src/adGeneration/wakeUpMerger.ts
+++ functions/src/adGeneration/wakeUpMerger.ts
@@ -4,8 +4,10 @@ export const wakeUpMerger = onRequest(async (request, response) => {
     const res = await fetch("https://merge-media-dx3v2rbg6q-od.a.run.app/wakeup");
 
     if (res.ok) {
+        console.log("Merger woken up successfully!");
         response.status(200).send("Success! " + await res.text());
     } else {
+        console.error("Error waking up merger! " + await res.text());
         response.status(500).send("Error waking up merger! " + await res.text());
     }
 });
